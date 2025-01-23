402ec1d214887d18fc3b3951409a6ee54e5385a7
Emilie <emilie.vlkova1@gmail.com>
2025-01-23 19:10:21 +0100

    Added PlayerData and added it in PlayerController, Organized asset folders

---
+++ Assets/_Scripts/ScriptableObjects/PlayerData/PlayerDataSo.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace _Scripts.ScriptableObjects.PlayerData
+{
+    [CreateAssetMenu(menuName = "Data/Player Data/Base Data", fileName = ("newPlayerData"))]
+    public class PlayerDataSo : ScriptableObject
+    {
+        [Header("Player Movement Speed")] 
+        public float playerMovementSpeed = 5f;
+    }
+}
