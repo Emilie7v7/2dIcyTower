a616b1ef48e76e88b78d2ad09e39fe8b535188d3
Tomas1564230 <Husivarga1412@gmail.com>
2025-01-27 19:53:49 +0100

    Projectile, and collision detection for Ground layer and Enemy layer

--- Assets/_Scripts/Projectile/Potion.cs
+++ Assets/_Scripts/Projectile/Potion.cs
@@ -1,65 +1,36 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-namespace _Scripts.Projectile
+public class NewScript : MonoBehaviour
 {
-    public class Potion : MonoBehaviour
+    public GameObject projectile;
+    [SerializeField] private float spawnOffset;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask enemyLayer;
+
+    void Update()
     {
-        [Header("Explosion Settings")]
-        public GameObject explosionPrefab; // Reference to the explosion prefab
-        private Rigidbody2D potionRb;
-
-        private void Awake()
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Get the Rigidbody2D component of the potion
-            potionRb = GetComponent<Rigidbody2D>();
+            Instantiate(projectile, transform.position + new Vector3(spawnOffset, spawnOffset, 0f), transform.rotation);
         }
+    }
 
-        private void Start()
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check for Ground layer
+        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
         {
-            // Ensure the potion collider is not a trigger initially (for normal collision)
-            Collider2D collider = GetComponent<Collider2D>();
-            if (collider != null)
-            {
-                collider.isTrigger = true; // Set the potion collider as trigger to pass through the player
-            }
-
-            // Ignore collision between the potion and the player
-            Collider2D playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
-            if (playerCollider != null)
-            {
-                Physics2D.IgnoreCollision(playerCollider, GetComponent<Collider2D>());
-            }
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            // Log what the potion is triggering with
-            Debug.Log("Potion triggered with: " + other.name);
-
-            // Ignore the player and trigger explosion if colliding with any other object
-            if (other.CompareTag("Player"))
-            {
-                Debug.Log("Potion passed through the player.");
-                return; // Ignore collision with the player
-            }
-
-            // Spawn the explosion object if not colliding with the player
-            SpawnExplosion();
-
-            // Destroy the potion
+            Debug.Log("Hit Ground.");
             Destroy(gameObject);
         }
 
-        private void SpawnExplosion()
+        // Check for Enemy layer
+        if (((1 << collision.gameObject.layer) & enemyLayer) != 0)
         {
-            if (explosionPrefab != null)
-            {
-                Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            }
-            else
-            {
-                Debug.LogError("Explosion prefab not assigned!");
-            }
+            Debug.Log("Hit Enemy.");
+            Destroy(gameObject);
         }
     }
 }
